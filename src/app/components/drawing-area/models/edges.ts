94e156fa463ebc924bd02ab811ca36df6d5a6e73
Daniel Haid <daniel.haid@studium.fernuni-hagen.de>
2024-11-23 11:53:44 +0100

    SCRUM-32: Fix calculation of arc length

--- src/app/components/drawing-area/models/edges.ts
+++ src/app/components/drawing-area/models/edges.ts
@@ -2,6 +2,12 @@ import { environment } from 'src/environments/environment';
 import { Activity, Place, Transition } from './nodes';
 
 export abstract class Edge {
+    readonly length: number;
+    readonly alphaRad: number;
+    readonly alphaDeg: number;
+    readonly sgnX: number;
+    readonly sgnY: number;
+
     x1: number = 0;
     x2: number = 0;
     y1: number = 0;
@@ -10,9 +16,78 @@ export abstract class Edge {
     constructor(
         readonly start: Place | Transition | Activity,
         readonly end: Place | Activity,
-    ) {}
+    ) {
+        const lengthX = this.end.x - this.start.x;
+        const lengthY = this.end.y - this.start.y;
+
+        this.length = Math.sqrt(Math.pow(lengthX, 2) + Math.pow(lengthY, 2));
+        this.alphaRad = Math.atan(lengthY / lengthX);
+        this.alphaDeg = (this.alphaRad / Math.PI) * 180;
+
+        this.sgnX = this.start.x > this.end.x ? -1 : 1;
+        this.sgnY = this.start.y > this.end.y ? -1 : 1;
+    }
 
     abstract calculateCoordinates(): void;
+}
+
+export class DfgArc extends Edge {
+    constructor(start: Activity, end: Activity) {
+        super(start, end);
+        this.calculateCoordinates();
+    }
+
+    override calculateCoordinates(): void {
+        const x1 = this.start.x;
+        const y1 = this.start.y;
+
+        const offsetArrow = 5;
+        const widthHalf = environment.drawingElements.activities.width / 2;
+        const heightHalf = environment.drawingElements.activities.height / 2;
+        const strokeHalf =
+            environment.drawingElements.activities.strokeWidth / 2;
+
+        let lengthToBorder = this.calculateLengthToActivityBorder(
+            widthHalf,
+            heightHalf,
+            strokeHalf,
+        );
+
+        const realLength = this.length - lengthToBorder - offsetArrow;
+        const deltaX = realLength * Math.cos(Math.abs(this.alphaRad));
+        const deltaY = realLength * Math.sin(Math.abs(this.alphaRad));
+
+        this.x1 = x1;
+        this.x2 = x1 + this.sgnX * deltaX;
+        this.y1 = y1;
+        this.y2 = y1 + this.sgnY * deltaY;
+    }
+
+    private calculateLengthToActivityBorder(
+        widthHalf: number,
+        heightHalf: number,
+        strokeHalf: number,
+    ): number {
+        const alphaDegAbs = Math.abs(this.alphaDeg);
+        switch (alphaDegAbs) {
+            case 0:
+                return widthHalf + strokeHalf;
+            case 45:
+                return Math.sqrt(
+                    Math.pow(widthHalf + strokeHalf, 2) +
+                        Math.pow(heightHalf + strokeHalf, 2),
+                );
+            case 90:
+                return heightHalf + strokeHalf;
+
+            default:
+                break;
+        }
+
+        return alphaDegAbs < 45
+            ? (widthHalf + strokeHalf) / Math.cos(Math.abs(this.alphaRad))
+            : (heightHalf + strokeHalf) / Math.sin(Math.abs(this.alphaRad));
+    }
 }
 
 export class PlaceToTransitionArc extends Edge {
@@ -22,20 +97,55 @@ export class PlaceToTransitionArc extends Edge {
     }
 
     override calculateCoordinates(): void {
-        const width: number = environment.drawingElements.transitions.width;
+        const x1 = this.start.x;
+        const y1 = this.start.y;
 
-        const refX: number = 5;
+        const offsetArrow = 5;
+        const widthHalf = environment.drawingElements.activities.width / 2;
+        const heightHalf = environment.drawingElements.activities.height / 2;
+        const strokeHalf =
+            environment.drawingElements.activities.strokeWidth / 2;
 
-        const lengthY = this.end.coordinates.y - this.start.coordinates.y;
-        const lengthX = this.end.coordinates.x - this.start.coordinates.x;
-        const alpha = Math.atan(lengthY / lengthX);
+        let lengthToBorder = this.calculateLengthToTransitionBorder(
+            widthHalf,
+            heightHalf,
+            strokeHalf,
+        );
 
-        const x2_: number = this.end.coordinates.x - width / 2;
-        const y2_: number = (width / 2) * Math.tan(-1 * alpha);
-        this.x1 = this.start.coordinates.x;
-        this.y1 = this.start.coordinates.y;
-        this.x2 = x2_ - refX;
-        this.y2 = this.end.coordinates.y + y2_;
+        const realLength = this.length - lengthToBorder - offsetArrow;
+        const deltaX = realLength * Math.cos(Math.abs(this.alphaRad));
+        const deltaY = realLength * Math.sin(Math.abs(this.alphaRad));
+
+        this.x1 = x1;
+        this.x2 = x1 + this.sgnX * deltaX;
+        this.y1 = y1;
+        this.y2 = y1 + this.sgnY * deltaY;
+    }
+
+    private calculateLengthToTransitionBorder(
+        widthHalf: number,
+        heightHalf: number,
+        strokeHalf: number,
+    ): number {
+        const alphaDegAbs = Math.abs(this.alphaDeg);
+        switch (alphaDegAbs) {
+            case 0:
+                return widthHalf + strokeHalf;
+            case 45:
+                return Math.sqrt(
+                    Math.pow(widthHalf + strokeHalf, 2) +
+                        Math.pow(heightHalf + strokeHalf, 2),
+                );
+            case 90:
+                return heightHalf + strokeHalf;
+
+            default:
+                break;
+        }
+
+        return alphaDegAbs < 45
+            ? (widthHalf + strokeHalf) / Math.cos(Math.abs(this.alphaRad))
+            : (heightHalf + strokeHalf) / Math.sin(Math.abs(this.alphaRad));
     }
 }
 
@@ -46,48 +156,22 @@ export class TransitionToPlaceArc extends Edge {
     }
 
     override calculateCoordinates(): void {
-        const radius: number = environment.drawingElements.places.radius;
+        const x1 = this.start.x;
+        const y1 = this.start.y;
 
-        const lengthY = this.end.coordinates.y - this.start.coordinates.y;
-        const lengthX = this.end.coordinates.x - this.start.coordinates.x;
-        const alpha = Math.atan(lengthY / lengthX);
+        const offsetArrow = 5;
+        const radius = environment.drawingElements.places.radius;
+        const strokeHalf = environment.drawingElements.places.strokeWidth / 2;
 
-        // const x2_: number = this.end.coordinates.x - radius - 5;
-        // const y2_: number = radius * Math.tan(-1 * alpha);
+        const lengthToBorder = radius + strokeHalf;
+        const realLength = this.length - lengthToBorder - offsetArrow;
 
-        const x_2: number = radius * Math.sin(alpha);
-        const y_2: number = radius * Math.cos(alpha);
+        const deltaX = realLength * Math.cos(Math.abs(this.alphaRad));
+        const deltaY = realLength * Math.sin(Math.abs(this.alphaRad));
 
-        this.x1 = this.start.coordinates.x;
-        this.y1 = this.start.coordinates.y;
-        // this.x2 = x2_;
-        this.x2 += x_2;
-        // this.y2 = this.end.coordinates.y + y2_;
-        this.y2 += y_2;
-    }
-}
-
-export class DfgArc extends Edge {
-    constructor(start: Activity, end: Activity) {
-        super(start, end);
-        this.calculateCoordinates();
-    }
-
-    override calculateCoordinates(): void {
-        const width: number = environment.drawingElements.activities.width;
-
-        const refX: number = 5;
-
-        const lengthY = this.end.coordinates.y - this.start.coordinates.y;
-        const lengthX = this.end.coordinates.x - this.start.coordinates.x;
-        const alpha = Math.atan(lengthY / lengthX);
-
-        const x2_: number = this.end.coordinates.x - width / 2;
-        const y2_: number = (width / 2) * Math.tan(-1 * alpha);
-
-        this.x1 = this.start.coordinates.x;
-        this.y1 = this.start.coordinates.y;
-        this.x2 = x2_ - refX;
-        this.y2 = this.end.coordinates.y + y2_;
+        this.x1 = x1;
+        this.x2 = x1 + this.sgnX * deltaX;
+        this.y1 = y1;
+        this.y2 = y1 + this.sgnY * deltaY;
     }
 }
