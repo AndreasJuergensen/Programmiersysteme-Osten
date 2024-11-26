631b6da41fa9e07237962d728c3436415f0e0639
Daniel Haid <daniel.haid@studium.fernuni-hagen.de>
2024-11-26 22:24:54 +0100

    SCRUM-27: Refactoring service

--- src/app/services/calculate-coordinates.service.ts
+++ src/app/services/calculate-coordinates.service.ts
@@ -1,9 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Dfg } from '../classes/dfg/dfg';
 import { Activities, Activity } from '../classes/dfg/activities';
-import { DrawingAreaComponent } from '../components/drawing-area';
-import { BehaviorSubject, last, Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { Graph, Node, Edge, StackElement } from '../classes/graph';
+import { DfgArc } from '../classes/dfg/arcs';
 
 @Injectable({
     providedIn: 'root',
@@ -18,27 +18,37 @@ export class CalculateCoordinatesService {
         return this._graph$.asObservable();
     }
 
-    private biggestYinNode(nodes: Node[]) {
-        const nodes2: Array<number> = nodes.map((node) => node.y);
-        return Math.max(...nodes2);
+    public calculateCoordinates(dfg: Dfg): void {
+        // Stage 1
+        const nodes: Array<Node> = this.generateNodes(dfg);
+        const dfgArcs: Array<DfgArc> = dfg.arcs.getArcs();
+        const edges: Array<Edge> = this.generateEdges(nodes, dfgArcs);
+
+        // Stage 2
+        this.recalculateCoordinatesForOverlaps(nodes, edges);
+
+        // Publish calculated graph
+        this._graph$.next({ nodes: nodes, edges: edges });
     }
 
-    public calculateCoordinates(dfg: Dfg): void {
-        const nodes: Array<Node> = [];
-        const edges: Array<Edge> = [];
+    /**
+     * Calculate coordinates of nodes with DFS
+     * @param {Dfg} dfg - Dfg for which nodes are calculated
+     * @returns {Array<Node>} Return an array of generated nodes
+     */
+    private generateNodes(dfg: Dfg): Array<Node> {
+        const nodes: Array<Node> = new Array<Node>();
 
         let yCoordinate: number = 100;
-        let lastX: number = 100;
+        let xOfLastModeledNode: number = 100;
 
         const playActivity: Activity = dfg.activities.getActivityByName('play');
         nodes.push({ id: playActivity.name, x: 100, y: 100 });
         const neighbours: Activities =
             dfg.arcs.calculateNextActivities(playActivity);
 
-        /*
-        Die Koordinaten des DFG werden mittels Tiefensuche von Graphen (DFS) die Koordinaten berechnet.
-        Der Stack enthält dabei die Elemente des Graphen, die schon gefunden wurden, deren Koordinanten aber noch berechnet werden müssen.
-        */
+        // Put all neighbours of start activity into stack with the coordinates
+        // of the start activity.
         const stack: Array<StackElement> = neighbours
             .getAllActivites()
             .map((neighbour) => {
@@ -47,42 +57,95 @@ export class CalculateCoordinatesService {
 
         while (stack.length > 0) {
             const stackElement: StackElement = stack.pop()!;
-            if (
-                nodes.find((node) => node.id === 'stop') !== undefined &&
-                lastX > stackElement.source_x
-            ) {
-                yCoordinate = this.biggestYinNode(nodes) + 100;
+            if (this.InsertNewLevel(nodes, stackElement, xOfLastModeledNode)) {
+                yCoordinate = this.biggestYCoodinateOfNodes(nodes) + 100;
             }
 
-            if (
-                nodes.find((node) => node.id === stackElement.activity.name) ===
-                undefined
-            ) {
-                const activityAsNode = {
-                    id: stackElement.activity.name,
-                    x: stackElement.source_x + 100,
-                    y: yCoordinate,
-                };
-                nodes.push(activityAsNode);
-
-                const neighbours: Activities = dfg.arcs.calculateNextActivities(
-                    stackElement.activity,
-                );
-
-                stack.push(
-                    ...neighbours.getAllActivites().map((neighbour) => {
-                        return {
-                            activity: neighbour,
-                            source_x: activityAsNode.x,
-                            source_y: activityAsNode.y,
-                        };
-                    }),
-                );
-                lastX = activityAsNode.x;
+            if (this.IsNodeAlreadyModeled(nodes, stackElement)) {
+                continue;
             }
+
+            const activityAsNode = {
+                id: stackElement.activity.name,
+                x: stackElement.source_x + 100,
+                y: yCoordinate,
+            };
+            nodes.push(activityAsNode);
+
+            const neighbours: Activities = dfg.arcs.calculateNextActivities(
+                stackElement.activity,
+            );
+
+            stack.push(
+                ...neighbours.getAllActivites().map((neighbour) => {
+                    return {
+                        activity: neighbour,
+                        source_x: activityAsNode.x,
+                        source_y: activityAsNode.y,
+                    };
+                }),
+            );
+            xOfLastModeledNode = activityAsNode.x;
         }
 
-        dfg.arcs.getArcs().forEach((arc) => {
+        return nodes;
+    }
+
+    /**
+     *
+     * @param {Array<Node>} nodes - Already calculated nodes
+     * @param {StackElement} stackElement - Current node from stack
+     * @param {number} xOfLastModeledNode - X-Value of last modeled node
+     * @returns {boolean} Return true if we have to insert a new level
+     */
+    private InsertNewLevel(
+        nodes: Array<Node>,
+        stackElement: StackElement,
+        xOfLastModeledNode: number,
+    ): boolean {
+        return (
+            nodes.find((node) => node.id === 'stop') !== undefined &&
+            xOfLastModeledNode > stackElement.source_x
+        );
+    }
+
+    /**
+     *
+     * @param {Array<Node>} nodes - Already calculated nodes
+     * @param {StackElement} stackElement - StackElement which is tested
+     * @returns {boolean} Returns true if node is already modeled
+     */
+    private IsNodeAlreadyModeled(
+        nodes: Array<Node>,
+        stackElement: StackElement,
+    ): boolean {
+        return (
+            nodes.find((node) => node.id === stackElement.activity.name) !==
+            undefined
+        );
+    }
+
+    /**
+     * Return the biggest y-coordinate of an array of nodes.
+     * @param {Array<Node>} nodes - Nodes with coordinates
+     * @returns {number} The maximum y-coordinate
+     */
+    private biggestYCoodinateOfNodes(nodes: Array<Node>): number {
+        return Math.max(...nodes.map((node) => node.y));
+    }
+
+    /**
+     *
+     * @param {Array<Node>} nodes - Nodes with calculated coordinates
+     * @param {Array<DfgArc} dfgArcs - Arcs for which edges have to be generated
+     * @returns {Array<Edge>} Returns array of generated edges by arcs
+     */
+    private generateEdges(
+        nodes: Array<Node>,
+        dfgArcs: Array<DfgArc>,
+    ): Array<Edge> {
+        const edges: Array<Edge> = new Array<Edge>();
+        dfgArcs.forEach((arc) => {
             const startNode: Node = nodes.find(
                 (node) => arc.getStart().name === node.id,
             )!;
@@ -93,72 +156,43 @@ export class CalculateCoordinatesService {
             edges.push({ source: startNode, target: endNode });
         });
 
-        this.nacharbeitKoordinaten(nodes, edges);
-
-        this._graph$.next({ nodes: nodes, edges: edges });
+        return edges;
     }
 
-    public nacharbeitKoordinaten(nodes: Array<Node>, edges: Array<Edge>) {
-        let runAgain: boolean;
+    /**
+     * Moves nodes on y-axis if an overlap between node and node or node and edge exists.
+     * @param {Array<Node>} nodes Array of all nodes with calculated coordinates
+     * @param {Array<Edge>} edges Array of all edges
+     */
+    public recalculateCoordinatesForOverlaps(
+        nodes: Array<Node>,
+        edges: Array<Edge>,
+    ) {
+        let nodeWasMoved: boolean;
 
         do {
-            runAgain = false;
-            nodes.forEach((element) => {
+            nodeWasMoved = false;
+            nodes.forEach((node) => {
                 if (
-                    this.pruefeUeberschneidungNodeAufNode(element, nodes) ||
-                    this.pruefeUeberschneidungNodeAufEdge(element, edges)
+                    this.checkNodeOverlap(node, nodes) ||
+                    this.checkEdgeOverlap(node, edges)
                 ) {
-                    //neue Koordinaten setzen
-                    element.y += 100;
-                    runAgain = true;
+                    node.y += 100;
+                    nodeWasMoved = true;
                 }
             });
-        } while (runAgain);
+        } while (nodeWasMoved);
     }
 
-    private pruefeUeberschneidungNodeAufNode(
-        element: Node,
-        nodes: Array<Node>,
-    ): boolean {
-        let ueberschneidendesElement: Node | undefined = nodes.find((node) => {
-            node.x === element.x &&
-                node.y === element.y &&
-                node.id !== element.id;
-        });
-
-        return ueberschneidendesElement !== undefined;
-    }
-
-    private pruefeUeberschneidungNodeAufEdge(
-        element: Node,
-        edges: Array<Edge>,
-    ): boolean {
-        for (const edge of edges) {
-            //Überprüfung, ob Node die Start- oder Ziel-Node des Arc ist
-            if (
-                edge.source.id === element.id ||
-                edge.target.id === element.id
-            ) {
-                continue;
-            }
-
-            //Überprüfung, ob die Node auf der Geraden liegt, die durch die Arc gebildet wird, soz. Überprüfung auf Abstand 0
-            if (edge.source.y === element.y && edge.target.y === element.y) {
-                //Überprüfung, ob die Node zwischen dem Start- und End-Punkt der Arc liegt und somit, ob die Node auf der Arc liegt
-                if (element.x < edge.target.x && element.x > edge.source.x) {
-                    //es muss verschoben werden, da die Node auf dem Arc liegt
-                    return true;
-                } else {
-                    //es muss nicht verschoben werden, da Node nicht auf Arc liegt
-                    continue;
-                }
-            }
-
-            let abstand = this.abstandBerechnen(edge, element);
-
-            // if (abstand < 15) {
-            if (abstand < 15 && abstand > 0) {
-                //der Abstand ist zu klein, deshalb müssen wir verschieben
+    /**
+     * Returns true if node overlaps an other node.
+     * @param {Node} node - The node for which the check is performed
+     * @param {Array<Node>} nodes - Array of all nodes to check node against
+     * @returns {boolean} Returns true if node overlaps an other node
+     */
+    private checkNodeOverlap(node: Node, nodes: Array<Node>): boolean {
+        for (const n of nodes) {
+            if (n.x === node.x && n.y === node.y && n.id !== node.id) {
                 return true;
             }
         }
@@ -166,12 +200,84 @@ export class CalculateCoordinatesService {
         return false;
     }
 
-    private abstandBerechnen(edge: Edge, node: Node): number {
-        let koeffizienten = this.berechneNormalform(edge);
+    /**
+     * Returns true if edge overlaps a node. This is measured by distance between edge and node.
+     * @param {Node} node - The node for which the check is perfomed
+     * @param {Array<Edge>} edges - Array of all edges to check the distance against
+     * @returns {boolean} Returns true if node must be moved
+     */
+    private checkEdgeOverlap(node: Node, edges: Array<Edge>): boolean {
+        for (const edge of edges) {
+            if (this.NodeBelongsToEdge(edge, node)) {
+                continue;
+            }
 
-        let a = koeffizienten[0];
-        let b = koeffizienten[1];
-        let c = koeffizienten[2];
+            if (this.IsEdgeAndNodeOnSameHorizontalLevel(edge, node)) {
+                if (this.IsNodeOnEdgeOnSameHorizontalLevel(edge, node)) {
+                    return true;
+                } else {
+                    continue;
+                }
+            }
+
+            let distance = this.calculateDistanceBetweenNodeAndEdge(edge, node);
+            if (0 < distance && distance < 15) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     *
+     * @param {Edge} edge - The edge for which the check is performed
+     * @param {Node} node - The node which is tested
+     * @returns {boolean} True if node is start or target
+     */
+    private NodeBelongsToEdge(edge: Edge, node: Node): boolean {
+        return edge.source.id === node.id || edge.target.id === node.id;
+    }
+
+    /**
+     *
+     * @param {Edge} edge - The edge for which the check is performed
+     * @param {Node} node - The node which is tested
+     * @returns {boolean} True if on same horizontal level
+     */
+    private IsEdgeAndNodeOnSameHorizontalLevel(
+        edge: Edge,
+        node: Node,
+    ): boolean {
+        return edge.source.y === node.y && edge.target.y === node.y;
+    }
+
+    /**
+     *
+     * @param {Edge} edge - The edge for which the check is performed
+     * @param {Node} node - The node which is tested
+     * @returns {boolean} True if node is on edge
+     */
+    private IsNodeOnEdgeOnSameHorizontalLevel(edge: Edge, node: Node): boolean {
+        return node.x < edge.target.x && node.x > edge.source.x;
+    }
+
+    /**
+     *
+     * @param {Edge} edge - The edge for which the distance is calculated
+     * @param {Node} node - The node for which the distance is calculated
+     * @returns {number} The distance between the given edge and node
+     */
+    private calculateDistanceBetweenNodeAndEdge(
+        edge: Edge,
+        node: Node,
+    ): number {
+        let coefficients =
+            this.calculateCanoncialLinearEquationCoefficients(edge);
+
+        let a = coefficients[0];
+        let b = coefficients[1];
+        let c = coefficients[2];
 
         return (
             Math.abs(a * node.x + b * node.y + c) /
@@ -179,16 +285,19 @@ export class CalculateCoordinatesService {
         );
     }
 
-    //Die Normalform einer Geraden lautet ax + by + c = 0
-    //Die Methode gibt hierbei die Werte für a, b und c in der Form [a,b,c] zurück
-    private berechneNormalform(edge: Edge): [number, number, number] {
-        //a
-        let steigung =
+    /**
+     * Calculates the coefficients of the canonical linear equation ax + by + c = 0.
+     * @param {Edge} edge - The edge to calculate the coefficients for
+     * @returns {[number, number, number]} Tuple of coefficients
+     */
+    private calculateCanoncialLinearEquationCoefficients(
+        edge: Edge,
+    ): [number, number, number] {
+        let gradient =
             (edge.target.y - edge.source.y) / (edge.target.x - edge.source.x);
 
-        //c
-        let schnittpunktMitYAchse = edge.source.y - steigung * edge.source.x;
+        let yAxisPoint = edge.source.y - gradient * edge.source.x;
 
-        return [steigung, -1, schnittpunktMitYAchse];
+        return [gradient, -1, yAxisPoint];
     }
 }
