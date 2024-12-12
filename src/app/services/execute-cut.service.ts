10b90003f1522ce55e792c20c3cc0d04a2f6e2f5
NKfabi <fabian.riede@gmail.com>
2024-12-12 20:16:46 +0100

    SCRUM-15: quick changes

--- src/app/services/execute-cut.service.ts
+++ src/app/services/execute-cut.service.ts
@@ -29,45 +29,42 @@ export class ExecuteCutService {
 
         if (!validCutOrNot) {
             console.log('kein valider Cut! Bitte nochmal probieren!'); // hier spaeter Aufruf des Feedback-Service
-        } else {
-            let eventLogs = new Array<EventLog>();
-
-            switch (selectedCut) {
-                case cutType.ExclusiveCut:
-                    eventLogs = dfg.getEventLog().splitByExclusiveCut(a1);
-                    break;
-                case cutType.SequenceCut:
-                    eventLogs = dfg.getEventLog().splitBySequenceCut(a2);
-                    break;
-                case cutType.ParallelCut:
-                    eventLogs = dfg.getEventLog().splitByParallelCut(a1);
-                    break;
-                case cutType.LoopCut:
-                    eventLogs = dfg.getEventLog().splitByLoopCut(a1, a2);
-                    break;
-            }
-
-            const subDfg1: Dfg = this.calculateDfgService.calculate(
-                eventLogs[0],
-            );
-
-            const subDfg2: Dfg = this.calculateDfgService.calculate(
-                eventLogs[1],
-            );
-
-            console.log('Cut durchgefuehrt!' + ' selected Cut: ' + selectedCut); // hier spaeter Aufruf des Feedback-Service
-
-            this._petriNetManagementService.updateByWhatEverCut(
-                selectedCut,
-                dfg,
-                subDfg1,
-                subDfg2,
-            );
-
-            console.log('Petri Netz aktualisiert!'); // hier spaeter Aufruf des Feedback-Service
-
-            // Petri Netz zurückgeben mit Übergabe des Original-DFG und der Teil-DFGs
+            return;
         }
+
+        let eventLogs = new Array<EventLog>();
+
+        switch (selectedCut) {
+            case cutType.ExclusiveCut:
+                eventLogs = dfg.getEventLog().splitByExclusiveCut(a1);
+                break;
+            case cutType.SequenceCut:
+                eventLogs = dfg.getEventLog().splitBySequenceCut(a2);
+                break;
+            case cutType.ParallelCut:
+                eventLogs = dfg.getEventLog().splitByParallelCut(a1);
+                break;
+            case cutType.LoopCut:
+                eventLogs = dfg.getEventLog().splitByLoopCut(a1, a2);
+                break;
+        }
+
+        const subDfg1: Dfg = this.calculateDfgService.calculate(eventLogs[0]);
+
+        const subDfg2: Dfg = this.calculateDfgService.calculate(eventLogs[1]);
+
+        console.log('Cut durchgefuehrt!' + ' selected Cut: ' + selectedCut); // hier spaeter Aufruf des Feedback-Service
+
+        this._petriNetManagementService.updateByWhatEverCut(
+            selectedCut,
+            dfg,
+            subDfg1,
+            subDfg2,
+        );
+
+        console.log('Petri Netz aktualisiert!'); // hier spaeter Aufruf des Feedback-Service
+
+        // Petri Netz zurückgeben mit Übergabe des Original-DFG und der Teil-DFGs
     }
 
     // Kanten / Cut an Service zur Ueberpruefung weitergeben
