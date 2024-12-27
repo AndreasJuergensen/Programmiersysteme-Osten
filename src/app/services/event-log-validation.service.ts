631571dfee266b9d875bf472f0e22514ef7ab95a
NKfabi <fabian.riede@gmail.com>
2024-12-27 12:08:56 +0100

    SCRUM-23: deleted unnecessary Patterns

--- src/app/services/event-log-validation.service.ts
+++ src/app/services/event-log-validation.service.ts
@@ -4,29 +4,17 @@ import { Injectable } from '@angular/core';
     providedIn: 'root',
 })
 export class EventLogValidationService {
-    /* [^\+]+ = 1 bis n beliebige Zeichen außer "+"
-    (?:\+[^\+]+)* = 0 bis n mal ("+" gefolgt von einem String bestehend aus 1 bis n beliebigen Zeichen außer "+") */
+    /* [^\+\s]+ = 1 bis n beliebige Zeichen außer "+" und Leerzeichen
+    ((?:\s[^\+\s]+)*|(?:\s?\+\s?[^\+\s]+)*(?:\s[^\+\s]+)*)* eine Alternative folgt
+        entweeder ein Leerzeichen gefolgt von einem String ohne Plus und Leerzeichen (0 bis n mal) ODER
+        ein Plus mit optionalen Leerzeichen gefolgt von einem String ohne Plus und Leerzeichen (0 bis n mal) 
+        gefolgt von einem Leerzeichen gefolgt einem String ohne Plus und Leerzeichen (0 bis n mal)*/
 
-    private readonly validEventLogPattern = /^[^\+]+(?:\+[^\+]+)*$/;
-    private readonly validEventLogPattern2 =
-        /^[^\+\s]+(?:\s[^\+\s]+)*(?:\s\+\s[^\+\s]+)*$/;
-    private readonly validEventLogPattern3 =
-        /^[^\+\s]+(?:\s[^\+\s]+)*(?:\s\+\s[^\+\s]+)*(?:\s[^\+\s]+)*$/;
-    private readonly validEventLogPattern4 =
-        /^[^\+\s]+(?:\s[^\+\s]+)*(?:\s?\+\s?[^\+\s]+)*$/;
-    private readonly validEventLogPattern5 =
-        /^[^\+\s]+(?:\s[^\+\s]+)*(?:\s?\+\s?[^\+\s]+)*$/;
-    private readonly validEventLogPattern6 =
-        /^[^\+\s]+(?:\s[^\+\s]+)*(?:\s?\+\s?[^\+\s]+)*(?:\s[^\+\s]+)*$/;
-    private readonly validEventLogPattern7 =
-        /^[^\+\s]+((?:\s[^\+\s]+)*(?:\s?\+\s?[^\+\s]+)*(?:\s[^\+\s]+)*)*$/;
-    private readonly validEventLogPattern8 =
-        /^[^\+\s]+(?:\s?[^\+\s]+)*(?:\s?\+\s?[^\+\s]+)*$/;
-    private readonly validEventLogPattern9 =
+    private readonly validEventLogPattern =
         /^[^\+\s]+((?:\s[^\+\s]+)*|(?:\s?\+\s?[^\+\s]+)*(?:\s[^\+\s]+)*)*$/;
     constructor() {}
 
     validateInput(input: string): boolean {
-        return this.validEventLogPattern9.test(input);
+        return this.validEventLogPattern.test(input);
     }
 }
