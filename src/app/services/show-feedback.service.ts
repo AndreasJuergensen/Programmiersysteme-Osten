e644cd1ab055a944607ce9350ff6b70583a74420
NKfabi <fabian.riede@gmail.com>
2025-01-30 22:10:43 +0100

    SCRUM-105: style changes 3.0

--- src/app/services/show-feedback.service.ts
+++ src/app/services/show-feedback.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { MatDialog } from '@angular/material/dialog';
 import { CustomSnackbarPopupComponent } from '../components/feedback-components/custom-snackbar-popup/custom-snackbar-popup.component';
 
 @Injectable({
@@ -9,16 +8,10 @@ import { CustomSnackbarPopupComponent } from '../components/feedback-components/
 export class ShowFeedbackService {
     constructor(private snackbar: MatSnackBar) {}
 
-    /*
-    kann in jeder(m) beliebigen Komponenten bzw. Service aufgerufen werden,
-    um individuell Success-/Error Popups zu erzeugen
-    Für den Fall eines Errors kann man optional noch eine detaillierte 
-    Beschreibung als string mitgeben 
-    */
     showMessage(message: string, isError: boolean): void {
         this.snackbar.openFromComponent(CustomSnackbarPopupComponent, {
             data: { message, isError },
-            duration: isError ? 0 : 0,
+            duration: isError ? 0 : 7000,
             panelClass: isError ? 'error-snackbar' : 'success-snackbar',
             horizontalPosition: 'right',
             verticalPosition: 'top',
